a3e87e42e39beff6a1864bbc99795c36331ddb61
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-13 12:11:58 -0800

    chore: Finished first part of activity

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace cgomez_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char ch; 
+            ch = 'A';
+            Console.WriteLine($"ch is storing the value : {ch}");
+
+            int ch_decimal;
+            ch_decimal = ch;
+            Console.WriteLine($"The decimal ch is: {ch_decimal}");
+
+            int toConvert;
+            Console.Write("Enter an integer: ");
+            toConvert = int.Parse(Console.ReadLine());
+
+            char asChar;
+            asChar = (Char)toConvert;
+            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+
+            char b;
+            b = (char)('A'+ 1);
+            Console.WriteLine($"'A' + 1 = '{b}'");  
+            
+
         }
     }
 }
