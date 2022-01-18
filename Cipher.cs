25ef81bd2509fb014d49849ff27b198d403256bb
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-18 10:04:05 -0800

    Push and COmmit

--- Cipher.cs
+++ Cipher.cs
@@ -43,7 +43,7 @@ public class Cipher
             newMessage += (char)(c + shift);
 
         }
-        return null;
+        return newMessage;
     }
 
 
--- Program.cs
+++ Program.cs
@@ -13,9 +13,11 @@ namespace cgomez_DataEncodingInCSharp
             encrypted = cipher.Encrypt("rosebud");
             Console.WriteLine($"The encrypted message is: '{encrypted}'");
 
+            
             string decrypted;
-            decrypted = cipher.Decrypt("rosebud");
-            Console.WriteLine($"The encrypted message is: '{decrypted}'");
+            decrypted = cipher.Decrypt(encrypted);
+            Console.WriteLine($"The decrypted message is: '{decrypted}'");
+
 
 
             
