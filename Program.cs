7ce374a6a3e53b90f86c163ca15e5ca6ae1d0785
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-17 16:46:40 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace cgomez_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Cipher cipher;
             cipher = new Cipher(5);
 
             string encrypted;
             encrypted = cipher.Encrypt("rosebud");
             Console.WriteLine($"The encrypted message is: '{encrypted}'");
+=======
+            Console.WriteLine("Hello World!");
+>>>>>>> parent of a3e87e4 (chore: Finished first part of activity)
         }
     }
 }
