db62f6f148bd0ea8a6ce38d495e00974ca5f01d0
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-18 10:25:58 -0800

    chore: Finish decrypting messages.

--- Program.cs
+++ Program.cs
@@ -13,14 +13,24 @@ namespace cgomez_DataEncodingInCSharp
             message = System.IO.File.ReadAllText(filePath);
             Console.WriteLine($"The encrypted message is: {message}");
 
-            Cipher cipher;
-            cipher = new Cipher(3);
+            // Cipher cipher;
+            // cipher = new Cipher(3);
 
             
-            string decrypted;
-            decrypted = cipher.Decrypt(message);
-            Console.WriteLine($"The decrypted message is: '{decrypted}'");
+            // string decrypted;
+            // decrypted = cipher.Decrypt(message);
+            // Console.WriteLine($"The decrypted message is: '{decrypted}'");
 
+
+            int shift = 1;
+            while (shift <= 10)
+            {
+                Cipher cipher;
+                cipher = new Cipher(shift);
+                string decrypt = cipher.Decrypt(message);
+                Console.WriteLine($"The decrypted message :'{decrypt}'.");
+                shift = shift + 1;
+            }
 
 
             
