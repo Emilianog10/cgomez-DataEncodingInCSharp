ea0d70f36c7c6f0b971d27220d78a739fa451319
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2022-01-17 17:59:44 -0800

    feat: Add Encrypt method

--- Program.cs
+++ Program.cs
@@ -6,20 +6,15 @@ namespace cgomez_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
             Cipher cipher;
             cipher = new Cipher(5);
 
             string encrypted;
             encrypted = cipher.Encrypt("rosebud");
             Console.WriteLine($"The encrypted message is: '{encrypted}'");
-=======
-            Console.WriteLine("Hello World!");
->>>>>>> parent of a3e87e4 (chore: Finished first part of activity)
-=======
-            Console.WriteLine("Hello World!");
->>>>>>> parent of a3e87e4 (chore: Finished first part of activity)
+
+            
+            
         }
     }
 }
